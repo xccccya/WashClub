2f0a0163fc4902171b0d1821b051231024fa5aeb
Yuuki <chen_nanzhi@qq.com>
2025-09-02 20:06:30 +0800

    优化主入口文件，新增中文语言支持和日期处理库，提升应用的本地化体验。

--- apps/web-admin/src/main.ts
+++ apps/web-admin/src/main.ts
@@ -2,8 +2,16 @@ import { createApp } from 'vue';
 import ElementPlus from 'element-plus';
 import 'element-plus/dist/index.css';
 import 'element-plus/theme-chalk/dark/css-vars.css';
+import zhCn from 'element-plus/es/locale/lang/zh-cn';
+import dayjs from 'dayjs';
+import 'dayjs/locale/zh-cn';
 import App from './App.vue';
 import router from './router';
 
-createApp(App).use(router).use(ElementPlus).mount('#app');
+dayjs.locale('zh-cn');
+
+createApp(App)
+	.use(router)
+	.use(ElementPlus, { locale: zhCn })
+	.mount('#app');
 
